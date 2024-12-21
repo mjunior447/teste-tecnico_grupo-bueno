75d7cf6ff21129d4d5d043fbffb3617a151dcf27
Mauricio Antero <mjunior447@gmail.com>
2024-12-20 23:53:34 -0300

    feat: initial database setup

---
+++ backend/database.js
@@ -0,0 +1,22 @@
+import mysql from "mysql2";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+async function getUsers() {
+  const [rows] = await pool.query("SELECT * FROM users");
+  return rows;
+}
+
+const users = await getUsers();
+console.log(users);
---
+++ backend/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE user_db;
+USE user_db;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO users (name, email)
+VALUES
+('adalberto', 'adalb@mail.com'),
+('jucelina', 'jucel@mail.com');
